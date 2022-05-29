d34c4bbf085b35a8c4073de11373e143b8f54cf9
yy0931 <54441600+yy0931@users.noreply.github.com>
2022-05-29 15:57:28 +0900

    Override ctrl+s if the user does not have write permission

--- README.md
+++ README.md
@@ -1,9 +1,22 @@
 # Save as Root in Remote - SSH
-This extension saves files with root privileges on Linux or MacOS environments connected with `Remote - SSH`,
+This extension saves files with root privileges on Linux or macOS environments connected with `Remote - SSH`,
 is an easy solution to [FileSystemProvider: no way of handling permissions issues #48659](https://github.com/microsoft/vscode/issues/48659).
 
 ## Usage
-Select `Save as Root` in the command palette (F1 or Ctrl+Shift+P or Cmd+Shift+P).
+This extension overrides ctrl+S (or cmd+S) to use the sudo command when the current user does not have write permission.
+
+![screenshot2](./screenshot2.gif)
+
+## If you are using ctrl+S for actions other than saving files
+(e.g. emacs extensions use ctrl+s for in-file searches), add the following code to [keybindings.json](https://code.visualstudio.com/docs/getstarted/keybindings#_advanced-customization) to disable the keybinding of this extension and use `Save as Root` in the command palette (F1 or Ctrl+Shift+P or Cmd+Shift+P) instead.
+
+```json
+{
+    "key": "ctrl+s",  // Replace this with "cmd+s" if you are using Mac.
+    "command": "-save-as-root.saveFile",
+    "when": "save-as-root.noWriteAccess"
+}
+```
 
 ![Screenshot](https://raw.githubusercontent.com/yy0931/save-as-root/main/screenshot.gif)
 
--- extension.js
+++ extension.js
@@ -1,6 +1,7 @@
 const vscode = require("vscode")
 const { execFile } = require("child_process")
 const os = require("os")
+const fs = require("fs")
 
 /** @returns {Promise<void>} */
 const sudoWriteFile = async (/** @type {string} */filename, /** @type {string} */content) => {
@@ -67,7 +68,8 @@ const sudoWriteFile = async (/** @type {string} */filename, /** @type {string} *
     })
 }
 
-exports.activate = (/** @type {vscode.ExtensionContext} */context) => {
+exports.activate = async (/** @type {vscode.ExtensionContext} */context) => {
+    // When the command "save-as-root.saveFile" is called, read the contents of the editor and save it to the file using the sudo command.
     context.subscriptions.push(vscode.commands.registerCommand("save-as-root.saveFile", async () => {
         // Check the status of the editor
         const editor = vscode.window.activeTextEditor
@@ -116,6 +118,26 @@ exports.activate = (/** @type {vscode.ExtensionContext} */context) => {
             console.error(err)
             await vscode.window.showErrorMessage(`[Save as Root] ${/** @type {Error} */(err).message}`)
         }
+
+    }))
+
+    // Override Ctrl+S if the current user does not have write permission to the active file.
+    const checkFilePermissionAndToggleCtrlSKeybinding = async (/** @type {vscode.TextEditor | undefined} */e) => {
+        if (e?.document.uri.scheme !== "file" || process.platform === "win32") {
+            await vscode.commands.executeCommand("setContext", "save-as-root.noWriteAccess", false)
+            return
+        }
+        try {
+            await fs.promises.access(e.document.uri.fsPath, fs.constants.W_OK)
+        } catch (err) {
+            await vscode.commands.executeCommand("setContext", "save-as-root.noWriteAccess", err instanceof Error && /** @type {Error & { code: string }} */(err).code === "EACCES")
+            return
+        }
+        await vscode.commands.executeCommand("setContext", "save-as-root.noWriteAccess", false)
+    }
+    await checkFilePermissionAndToggleCtrlSKeybinding(vscode.window.activeTextEditor)
+    context.subscriptions.push(vscode.window.onDidChangeActiveTextEditor(async (e) => {
+        await checkFilePermissionAndToggleCtrlSKeybinding(e)
     }))
 }
 
